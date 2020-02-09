aa0beff5dda7bfff5584b071f40fb0336696686c
Oron Ben Zvi <oronbz@gmail.com>
2020-02-09 21:46:02 +0200

    Initial Commit

---
+++ CoreDataSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataSwiftUI
+//
+//  Created by Oron Ben Zvi on 2/9/20.
+//  Copyright © 2020 Oron Ben Zvi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
